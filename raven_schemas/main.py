4ac1439b183f7b99c0a8d762c46b644e283d5dc9
Jason Curtis <jason@optionzero.co>
2023-11-11 10:38:05 -0800

    make find_valid_versions() work

--- raven_schemas/main.py
+++ raven_schemas/main.py
@@ -2,42 +2,74 @@ import json
 import os
 from enum import Enum
 from pathlib import Path
-
-import jsonschema
+from typing import List
 
 import click
+import jsonschema
 
 PACKAGE_DIR = Path(os.path.dirname(__file__))
 
+
 class SchemaName(Enum):
-    modeling_input = 'modeling_input'
+    modeling_input = "modeling_input"
 
 
 def validate_json_single_version(data: dict, schema_name: SchemaName, version: str):
     """Validate the JSON schema for the given schema name at a specific version"""
     version_for_filename = version.replace(".", "_")
-    schema_path = PACKAGE_DIR / "schemas" / schema_name.value / f"{schema_name.value}_{version_for_filename}.json"
+    schema_path = (
+        PACKAGE_DIR
+        / "schemas"
+        / schema_name.name
+        / f"{schema_name.value}_{version_for_filename}.json"
+    )
     with open(schema_path) as f:
         schema = json.load(f)
     return jsonschema.validate(schema=schema, instance=data)
 
-def validate_json(data: dict, schema_name: SchemaName, versions: List[str]):
-    """Validate the JSON schema for the given schema name at a specific version"""
-    errors = [] # tuples of (version, message)
+
+def find_valid_versions(
+    json_data: dict, schema_name: SchemaName, versions: List[str]
+) -> List[str]:
+    """Find the first version in the list of versions that successfully validates
+    @raises ValueError if none of the versions validate.
+    """
+    errors = []  # tuples of (version, message)
+    valid_versions = []
     for version in versions:
         try:
-            validate_json_single_version(json_data, SchemaName[schema_name], version)
-            except jsonschema.exceptions.ValidationError as e:
-                e.append({'version': version, 'message': e.message})
-            except OSError as e:
+            validate_json_single_version(json_data, schema_name, version)
+        except jsonschema.exceptions.ValidationError as e:
+            errors.append({"version": version, "message": e.message})
+        except OSError:
+            raise ValueError(f"Schema {schema_name} version {version} not found")
+        else:
+            # Successfully validated
+            valid_versions.append(version)
+    if valid_versions:
+        return valid_versions
+    if errors:
+        raise ValueError(
+            f"Errors validating {schema_name}:\n"
+            + "\n".join([f"Version {e['version']}: {e['message']}" for e in errors])
+        )
+    raise ValueError(
+        f"Errors validating {schema_name}: no versions validated but no errors raised"
+    )
 
 
 @click.group()
 def raven_schemas():
     pass
 
+
 @raven_schemas.command()
-@click.option("-s", "--schema-name", type=click.Choice([s.name for s in SchemaName]), required=True)
+@click.option(
+    "-s",
+    "--schema-name",
+    type=click.Choice([s.name for s in SchemaName]),
+    required=True,
+)
 @click.option("-v", "--schema-version", multiple=True, required=True)
 @click.option("-f", "--json-file", type=click.Path(exists=True), required=True)
 def validate_file(schema_name: str, schema_version: List[str], json_file: Path):
@@ -45,9 +77,5 @@ def validate_file(schema_name: str, schema_version: List[str], json_file: Path):
     with open(json_file) as f:
         json_data = json.load(f)
 
-    errors = []
-    for version in schema_version:
-        try:
-            validate_json(json_data, SchemaName[schema_name], version)
-            except jsonschema.exceptions.ValidationError as e:
-                e.append({'version': version, 'message': e.message})
+    versions = find_valid_versions(json_data, SchemaName[schema_name], schema_version)
+    print(f"Successfully validated {schema_name} version(s): {versions}")
