3f27924f44dc340d6a940d78acd2e4ee6e5ca0f8
Keven Chhen <149608364+keven-chhen@users.noreply.github.com>
2024-03-28 12:45:32 -0700

    ER-793 inject solar panels to inspection survey (#20)
    * ER-793 inject solar panels to inspection survey
    
    * ER-793 updated type for solar panels present

---
+++ raven_schemas/modeling_input_1_1_6_test.py
@@ -0,0 +1,25 @@
+import json
+
+import jsonschema
+import pytest
+
+from raven_schemas import validate
+from raven_schemas.constants import PACKAGE_DIR
+
+
+@pytest.fixture
+def valid_1_1_6_modeling_json():
+    with open(
+        PACKAGE_DIR / "schemas/modeling_input_1_1_6_sample_valid.json"
+    ) as modeling_input:
+        return json.load(modeling_input)
+
+
+def test_invalid_survey_without_solar_panels(valid_1_1_6_modeling_json):
+
+    valid_1_1_6_modeling_json["survey"]["structure"]["solar_panels"] = None
+
+    with pytest.raises(jsonschema.exceptions.ValidationError):
+        validate.validate_json_single_version(
+            valid_1_1_6_modeling_json, "modeling_input", "1.1.6"
+        )
