e6dd2434ce17bfd50fc8c14ad4f7f99e5d9c4824
Keven Chhen <149608364+keven-chhen@users.noreply.github.com>
2024-03-29 11:20:23 -0700

    Changed schema version 1.1.6 to 1.2.0 (#22)

--- raven_schemas/modeling_input_1_2_0_test.py
+++ raven_schemas/modeling_input_1_2_0_test.py
@@ -8,18 +8,18 @@ from raven_schemas.constants import PACKAGE_DIR
 
 
 @pytest.fixture
-def valid_1_1_6_modeling_json():
+def valid_1_2_0_modeling_json():
     with open(
-        PACKAGE_DIR / "schemas/modeling_input_1_1_6_sample_valid.json"
+        PACKAGE_DIR / "schemas/modeling_input_1_2_0_sample_valid.json"
     ) as modeling_input:
         return json.load(modeling_input)
 
 
-def test_invalid_survey_without_solar_panels(valid_1_1_6_modeling_json):
+def test_invalid_survey_without_solar_panels(valid_1_2_0_modeling_json):
 
-    valid_1_1_6_modeling_json["survey"]["structure"]["solar_panels"] = None
+    valid_1_2_0_modeling_json["survey"]["structure"]["solar_panels"] = None
 
     with pytest.raises(jsonschema.exceptions.ValidationError):
         validate.validate_json_single_version(
-            valid_1_1_6_modeling_json, "modeling_input", "1.1.6"
+            valid_1_2_0_modeling_json, "modeling_input", "1.2.0"
         )
